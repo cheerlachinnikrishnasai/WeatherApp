37f51a0628ffd01cc5f51b6e156fefdf56a446d3
cheerlachinnikrishnasai <68896860+cheerlachinnikrishnasai@users.noreply.github.com>
2022-08-25 10:50:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useState } from 'react'
+import "./App.css"
+
+const App = () => {
+  const [city, setCity] = useState("")
+  const [result,setResult] =useState("")
+
+  const changeHandler = (e) => {
+    setCity(e.target.value)
+  }
+  
+  const submitHandler = (e) =>{
+    e.preventDefault();
+    //console.log(city)
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e1bc2351efd2d8345c2a386cda95e8b3&units=metric`)
+      .then(response => 
+        response.json()
+      ).then((data) =>{
+        const cityNameInApi = data.name
+        if(city !== cityNameInApi)
+        {
+          setResult(`Please provide valid city name`)
+        }
+        else{
+          setResult("The Temperature at " + city + " is " +  data.main.temp + "°C")
+        }
+      })
+      setCity("")
+    }
+  return (
+    <div className='App'>
+      <center>
+        <br />
+        <br />
+        <h2>
+          <b>Weather App</b>
+        </h2>
+        <br/>
+        <form onSubmit={submitHandler}>
+          <input className = 'inputField' type="text" size={30} name="city" value={city} onChange = {changeHandler} />
+          <br />
+          <br />
+          <button className = 'submitField' type="submit">Get Temperature</button>
+          <br />
+          <br />
+          <div className='result'>
+            <i><big><font size="+8" >{result}</font></big></i>
+          </div>
+        </form>
+        <footer>Note: For Tirupati Weather Report , Please type the input field as "Tirumala - Tirupati"</footer>
+      </center>
+    </div>
+  )
+}
+
+export default App
+
